4e2d869e382a8dffc049a7b6bb37d537dabab810
Roman Nasyrov <roman.nasyrov@gmail.com>
2024-05-07 17:41:18 +0500

    another major commit

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-from django.shortcuts import render
+    from django.shortcuts import render
 
 # Create your views here.
 def index(request):
